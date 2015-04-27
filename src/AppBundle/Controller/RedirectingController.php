e708ab5ca27a97d7d7f0f1261713e61e7639ea95
Amarjeet Singh Rai <git@arai.dev>
2015-04-27 13:14:40 +0100

    Add magic wallpaper redirect

--- src/AppBundle/Controller/RedirectingController.php
+++ src/AppBundle/Controller/RedirectingController.php
@@ -7,6 +7,26 @@ use Symfony\Component\HttpFoundation\Request;
 
 class RedirectingController extends Controller
 {
+    public function wallpaperRedirectAction($name)
+    {
+        $basePath = $this->get('kernel')->getBundle('AppBundle')->getPath().'/Resources/public/wallpaper/';
+        $path = $basePath.$name;
+
+        if (!file_exists($path)) {
+            $wallpaperService = $this->get('app.bing_wallpaper');
+            $wallpaperRepo    = $this->get('app.bing_wallpaper_repository');
+
+            $cleanName = $wallpaperService->cleanTitle($name);
+            $wallpaper = $wallpaperRepo->findOneLikeName($cleanName);
+
+            if ($wallpaper !== null && file_exists($basePath.$wallpaper->getName().'.jpg')) {
+                return $this->redirect('/wallpaper/'.$wallpaper->getName().'.jpg');
+            }
+        }
+
+        throw $this->createNotFoundException('No wallpaper found');
+    }
+
     public function removeTrailingSlashAction(Request $request)
     {
         $pathInfo = $request->getPathInfo();
