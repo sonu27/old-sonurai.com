455294b281dc5144bd60e5c877a947e5596836fe
Amarjeet Rai <jatt27@gmail.com>
2017-12-28 20:43:46 +0000

    Add tests

---
+++ tests/Service/BingWallpaperUpdaterTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Repository\BingWallpaperRepository;
+use App\Service\BingWallpaperUpdater;
+use PHPUnit\Framework\TestCase;
+
+class BingWallpaperUpdaterTest extends TestCase
+{
+    protected $obj;
+    protected $routeName = 'fake_route';
+
+    protected function setUp()
+    {
+        $wallpaperRepoProphecy = $this->prophesize(BingWallpaperRepository::class);
+
+        $this->obj = new BingWallpaperUpdater($wallpaperRepoProphecy->reveal());
+    }
+
+    public function testCleanTitleRemoveString()
+    {
+        $url = '/az/hprichbg/rb/';
+
+        $this->assertEquals('', $this->obj->cleanTitle($url));
+    }
+
+    public function testCleanTitleOnlyReturnsName()
+    {
+        $url = 'EasterIslandSmiles_EN-US8811665215';
+
+        $this->assertEquals('EasterIslandSmiles', $this->obj->cleanTitle($url));
+    }
+}
---
+++ tests/Service/PaginationTest.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Tests\Service;
+
+use App\Service\Pagination;
+use PHPUnit\Framework\TestCase;
+use Prophecy\Argument;
+use Symfony\Component\Routing\RouterInterface;
+
+class PaginationTest extends TestCase
+{
+    protected $obj;
+    protected $routeName = 'fake_route';
+
+    protected function setUp()
+    {
+        $routerProphecy = $this->prophesize();
+        $routerProphecy->willExtend('stdClass');
+        $routerProphecy->willImplement(RouterInterface::class);
+        $routerProphecy->generate(Argument::any(), Argument::any())->willReturn('/fake-url');
+
+        $this->obj = new Pagination($routerProphecy->reveal());
+    }
+
+    public function testOutputIsAnEmptyArrayWhenCountIsLessThanOne()
+    {
+        $pagination = $this->obj->paginate(0, $this->routeName, 1, 2);
+
+        $this->assertEquals([], $pagination);
+    }
+
+    /**
+     * @expectedException \LogicException
+     */
+    public function testLogicExceptionIsThrownWhenPageCannotExist()
+    {
+        $pagination = $this->obj->paginate(10, $this->routeName, 3, 5);
+
+        $this->assertEquals([], $pagination);
+    }
+
+    public function testArrayOutput1()
+    {
+        $expected = [
+            [
+                'page'  => 1,
+                'url'   => '/fake-url',
+                'class' => 'active',
+            ],
+            [
+                'page'  => 2,
+                'url'   => '/fake-url',
+                'class' => false,
+            ],
+            [
+                'page'  => '&raquo;',
+                'url'   => '/fake-url',
+                'class' => false,
+            ],
+        ];
+
+        $pagination = $this->obj->paginate(3, $this->routeName, 1, 2);
+
+        $this->assertEquals($expected, $pagination);
+    }
+
+    public function testArrayOutputWhenPageIsMoreThanOne()
+    {
+        $expected = [
+            [
+                'page'  => '&laquo;',
+                'url'   => '/fake-url',
+                'class' => false,
+            ],
+            [
+                'page'  => 1,
+                'url'   => '/fake-url',
+                'class' => false,
+            ],
+            [
+                'page'  => 2,
+                'url'   => '/fake-url',
+                'class' => 'active',
+            ],
+        ];
+
+        $pagination = $this->obj->paginate(3, $this->routeName, 2, 2);
+
+        $this->assertEquals($expected, $pagination);
+    }
+}
